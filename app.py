bb40dc5b30f2b2b88ad0b4a89d7e6e8a5020fd3b
David_Lane <davidlanemca@gmail.com>
2020-12-08 00:37:18 -0600

    Setup DogConatiner

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, g
+
+import models
 
 DEBUG = True
 PORT = 8001
@@ -7,15 +9,39 @@ PORT = 8001
 # This starts the website!
 app = Flask(__name__)
 
+
+@app.before_request
+def before_request():
+    "Connect to the database before each request."
+    g.db = models.DATABASE
+    g.db.connect()
+
+
+@app.after.request
+def after_request(response):
+    'Close the database connection after each request.'
+    g.db.close()
+    return response
+
 # The default URL ends in / ("my-website.com/").
 
 
 @app.route('/')
 def index():
-    my_list = ["Hey", "check", "this", "out"]
-    return my_list[0]
+    return 'hi'
+
+
+# @app.route('/sayhi/<username>')  # When someone goes here...
+# def hello(username):  # Do this.
+#     return "Hello {}".format(username)
+
+
+# @app.route('/json')
+# def song():
+#     return jsonify(title="Frankie", artist="Lionel")
 
 
 # Run the app when the program starts!
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=DEBUG, port=PORT)
